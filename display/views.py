6c2916c3c6ee7c54eabcdbde53d569353e82254a
Azazel5 <subhanga2013@gmail.com>
2020-04-23 22:34:21 -0500

    Feature complete

--- display/views.py
+++ display/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from .models import ProjectModel
 from django.views.generic import TemplateView
+from django.views.generic.list import ListView
 
-class HomeView(TemplateView):
-    template_name = 'display/index.html'
+class HomeView(ListView):
     model = ProjectModel
+    template_name = 'display/index.html'
+    context_object_name = 'projects'
 
 class BlogView(TemplateView):
     template_name = 'display/blog.html'
